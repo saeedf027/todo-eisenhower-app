a9baf9667a55aac7944c244d44ad112f64a9b702
saeedf027 <saeedforati092@gmail.com>
2026-05-15 13:12:29 +0430

    Add project files

---
+++ database.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 01:44:20 2026
+
+@author: fuji
+"""
+
---
+++ models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 12:51:37 2026
+
+@author: fuji
+"""
+
